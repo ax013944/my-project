5deea8256076c9c4f797028c8089ffdf74a20631
ax013944 <ax013944@gmail.com>
2017-10-03 16:41:26 +0800

    first commit

---
+++ src/com/well/hello.java
@@ -0,0 +1,11 @@
+package com.well;
+
+public class hello {
+
+	public static void main(String[] args) {
+
+		person p = new person();
+				p.sayhello();
+        
+	}
+}
---
+++ src/com/well/person.java
@@ -0,0 +1,14 @@
+package com.well;
+
+public class person {
+
+	public void sayhello(){
+System.out.println("hello");}
+	
+	public static void main(String[] args) {
+
+		person p = new person();
+				p.sayhello();
+        
+}
+}
